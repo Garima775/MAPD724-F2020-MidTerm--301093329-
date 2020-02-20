27d7bea72c1bddadb91d642cf06dcdb736f9a0dc
Garima <garimaprashar775@gmail.com>
2020-02-19 20:37:32 -0500

    added internal documentation

--- MAPD724-W2020-MidTerm-Template/Island.swift
+++ MAPD724-W2020-MidTerm-Template/Island.swift
@@ -1,3 +1,7 @@
+//Source file name- MAPD724-W2020-F2020-MideTerm-301093329
+//AUthors name- Garima Prasher
+//Student Number- 301093329
+//Test Date- 19 Feb 2020
 import SpriteKit
 import GameplayKit
 
